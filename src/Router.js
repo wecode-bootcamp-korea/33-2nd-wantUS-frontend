000fcdf2ae2bfff31f438701bfd21894589da471
Yumjong <yumjongeun@gmail.com>
2022-06-08 00:38:16 +0900

    'Modify:백엔드와 통신 1차'

--- src/Router.js
+++ src/Router.js
@@ -3,16 +3,18 @@ import Signup from './pages/signup/Signup';
 import Login from './pages/login/Login';
 import JobList from './pages/joblist/JobList';
 import JobDetail from './pages/jobdetail/JobDetail';
+import LikePage from './pages/likePage/LikePage';
 import Resume from './pages/resume/Resume';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/likePage" element={<LikePage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/joblist" element={<JobList />} />
-        <Route path="/jobdetail" element={<JobDetail />} />
+        <Route path="/jobdetail/:id" element={<JobDetail />} />
         <Route path="/resume" element={<Resume />} />
       </Routes>
     </BrowserRouter>
